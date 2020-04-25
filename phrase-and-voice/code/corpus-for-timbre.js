374d179da3326ba5851990fbb8167ee6c848d148
jamesstaub <james.staub@gmail.com>
2020-04-25 17:46:09 -0400

    adds corpus for timbre clustering method to vary the KNN sample choice based on input timbre

---
+++ phrase-and-voice/code/corpus-for-timbre.js
@@ -0,0 +1,24 @@
+
+
+function list()
+{
+	var a = arrayfromargs(arguments);
+	var thresh = this.threshold;
+	
+	a = a.map(function(weight, i){
+		return [weight, i];
+	})
+	.filter(function(weightIndex){ 
+		var w = weightIndex[0];
+		post(w + " \n");
+		post(thresh + " \n");
+		return w > thresh;
+	})
+	.map(function(weightIndex){
+		return weightIndex[1] + 1 // timbre ids are 1 indexed
+	});
+	
+	
+	outlet(0, a);
+	
+}
